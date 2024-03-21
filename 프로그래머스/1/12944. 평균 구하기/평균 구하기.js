80833c92a3ad094d0a3e0d0e3d163b1b55709f51
limdong123 <96884089+limdong123@users.noreply.github.com>
2024-03-21 14:50:24 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,11 +1,11 @@
 function solution(arr) {
     var answer = 0;
     
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 0 ; i < arr.length; i++){
         answer += arr[i]
     }
     
-    answer /= arr.length
-        
+    answer = answer/arr.length
+    
     return answer;
 }
