d9acbd2fb0cc8344f231d01b28c5684cf2dea828
limdong123 <96884089+limdong123@users.noreply.github.com>
2024-03-21 16:03:07 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,8 @@
 function solution(a, b) {
     var answer = 0;
     
-    for(let i = 0; i < a.length; i++){
-        answer += a[i] * b[i]
+    for(let i = 0; i< a.length; i++){
+        answer += a[i]*b[i]
     }
     
     return answer;
