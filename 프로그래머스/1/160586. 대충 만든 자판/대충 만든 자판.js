8cff5e217c70da716c4788b510bd1bbf14a63449
limdong123 <96884089+limdong123@users.noreply.github.com>
2024-05-29 14:19:13 +0900

    [level 1] Title: 대충 만든 자판, Time: 7.75 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,23 +1,10 @@
 function solution(keymap, targets) {
     var answer = [];
     
-    //["ABACD", "BCEFD"]	["ABCD","AABB"]	[9, 4]
-    //1 + 1 + 2 + 5
-    //1 + 1 + 1 + 1
-    //["AA"]	["B"]	[-1]
-    //불가능
-    //["AGZ", "BSSS"]	["ASA","BGZ"]	[4, 6]
-    //1 + 2 + 1
-    //1 + 2 + 3
-    
-    //배열 두개 순회 
-    //->원하는 알파벳나오면 그 인덱스 answer에 추가(둘다 돌았는데 안나오면 -1)  
-    //->출력
-    
     let text = ""
     let answer_num = 0
-    for(let i = 0; i < targets.length; i++){ //targets 도는 배열
-        for(let j = 0; j < targets[i].length; j++){ //targets[i]의 알파벳 순환 배열 
+    for(let i = 0; i < targets.length; i++){ 
+        for(let j = 0; j < targets[i].length; j++){ 
             text = targets[i][j] 
             let num = 101
             for(let k = 0; k < keymap.length; k++){
